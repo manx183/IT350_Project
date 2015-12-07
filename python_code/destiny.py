2d50fe3f123074d31dfe92a4dc92f8cbe437d86d
Hector Lugo <hector.d.lugo@gmail.com>
2015-12-06 18:41:53 -0700

    Update destiny.py
    Working triggers and almost all tables

--- python_code/destiny.py
+++ python_code/destiny.py
@@ -8,43 +8,49 @@ con = mdb.connect('localhost', 'hlugo', 'pass12345', 'destiny');
 with con:
 
 	cur = con.cursor()
-	cur.execute("DROP TABLE IF EXISTS Clan")
-	cur.execute("CREATE TABLE Clan(clanName VARCHAR(35) PRIMARY KEY, Size INT, Founder VARCHAR(20), Motto VARCHAR(50))")
-	cur.execute("INSERT INTO Clan(clanName, Size, Founder, Motto) VALUES('Lords of War', 5, 'WildHeck', 'Never Stop Shooting')")
+#Clan +
+	cur.execute("CREATE TABLE Clan(clanName VARCHAR(35) PRIMARY KEY, Size INT NOT NULL, Founder VARCHAR(20) NOT NULL, Motto VARCHAR(50))")
+#Server  +		
+	cur.execute("CREATE TABLE Server(serverID INT PRIMARY KEY, Location VARCHAR(25) NOT NULL, Status VARCHAR(7) NOT NULL)")
+#Player	+
+	cur.execute("CREATE TABLE Player(gamerID VARCHAR(20) PRIMARY KEY, serverID INT NOT NULL, clanName VARCHAR(35), firstName VARCHAR(12) NOT NULL, lastName VARCHAR(18) NOT NULL, DOB DATE NOT NULL, age INT, gender VARCHAR(6) NOT NULL, FOREIGN KEY (serverID) REFERENCES Server(serverID), FOREIGN KEY (clanName) REFERENCES Clan(clanName))")
+#pCharacter	
+	cur.execute("CREATE TABLE pCharacter(gamerID VARCHAR(20) NOT NULL, FOREIGN KEY(gamerID) REFERENCES Player(gamerID), characterNum INT(1) NOT NULL,level INT NOT NULL, race VARCHAR(6) NOT NULL, gender VARCHAR(6) NOT NULL, class VARCHAR(7) NOT NULL, light INT DEFAULT 0, UNIQUE(gamerID, characterNum))")
+#Vault
+	cur.execute("CREATE TABLE Vault(gamerID VARCHAR(20) NOT NULL, FOREIGN KEY(gamerID) REFERENCES Player(gamerID), glimmer INT, legendaryMarks INT, silver INT)")
+#Vault Items
+	cur.execute("CREATE TABLE VaultItems(gamerID VARCHAR(20) NOT NULL, FOREIGN KEY(gamerID) REFERENCES Player(gamerID), itemName VARCHAR(15), quantity int(3))")
+#LegArmor	+
+	cur.execute("CREATE TABLE LegArmor(legName VARCHAR(40), class VARCHAR(7), PRIMARY KEY(legName, class), rarity VARCHAR(10), def INT, dis INT, inte INT, str INT)")
+#Gauntlets	+
+	cur.execute("CREATE TABLE Gauntlets(gauntletName VARCHAR(40), class VARCHAR(7), PRIMARY KEY(gauntletName, class), rarity VARCHAR(10), def INT, dis INT, inte INT, str INT)")
+#ChestArmor	+
+	cur.execute("CREATE TABLE ChestArmor(chestName  VARCHAR(40), class VARCHAR(7), PRIMARY KEY(chestName, class), rarity VARCHAR(10), def INT, dis INT, inte INT, str INT)")
+#Helmet	+
+	cur.execute("CREATE TABLE Helmet(helmName VARCHAR(40), class VARCHAR(7), PRIMARY KEY(helmName, class), rarity VARCHAR(10), def INT, dis INT, inte INT, str INT)")
+#PrimaryWeapons  +
+	cur.execute("CREATE TABLE PrimaryWeapons(pName VARCHAR(25) PRIMARY KEY, rarity VARCHAR(10), subtype VARCHAR(20), attack INT)")
+#SpecialWeapons  +
+	cur.execute("CREATE TABLE SpecialWeapons(sName VARCHAR(25) PRIMARY KEY, rarity VARCHAR(10), subtype VARCHAR(20), attack INT)")
+#HeavyWeapons  +
+	cur.execute("CREATE TABLE HeavyWeapons(hName VARCHAR(25) PRIMARY KEY, rarity VARCHAR(10), subtype VARCHAR(20), attack INT)")
+#Quests  +
+	cur.execute("CREATE TABLE Quests(title VARCHAR(35) PRIMARY KEY, steps INT, description VARCHAR(140), category VARCHAR(30))")
+#Quest Rewards   +
+	cur.execute("CREATE TABLE QuestRewards(title VARCHAR(35), reward VARCHAR(35), FOREIGN KEY(title) REFERENCES Quests(title), UNIQUE(title, reward))")
+#InProgress Multivalued Attributes
+	cur.execute("CREATE TABLE InProgress(gamerID VARCHAR(20), title VARCHAR(20), FOREIGN KEY (gamerID) REFERENCES Player(gamerID), FOREIGN KEY (title) REFERENCES Quests(title))")
+#equipedWeapons
+	cur.execute("CREATE TABLE equipedWeapons(gamerID VARCHAR(20), FOREIGN KEY (gamerID) REFERENCES Player(gamerID), characterNum INT, UNIQUE(gamerID, characterNum), pName VARCHAR(25), FOREIGN KEY (pName) REFERENCES PrimaryWeapons(pName), sName VARCHAR(25), FOREIGN KEY (sName) REFERENCES SpecialWeapons(sName), hName VARCHAR(25), FOREIGN KEY (hName) REFERENCES HeavyWeapons(hName), wLight INT DEFAULT 0)")
+#equipedArmor
+	cur.execute("CREATE TABLE equipedArmor(gamerID VARCHAR(20), FOREIGN KEY (gamerID) REFERENCES Player(gamerID), characterNum INT, UNIQUE(gamerID, characterNum), class VARCHAR(7), helmName VARCHAR(40), FOREIGN KEY (helmName) REFERENCES Helmet(helmName), gauntletName VARCHAR(40), FOREIGN KEY (gauntletName) REFERENCES Gauntlets(gauntletName), chestName VARCHAR(40), FOREIGN KEY (chestName) REFERENCES ChestArmor(chestName), legName VARCHAR(40), FOREIGN KEY (legName) REFERENCES LegArmor(legName), aLight INT DEFAULT 0)")
 
-	cur.execute("DROP TABLE IF EXISTS Server")
-	cur.execute("CREATE TABLE Server(serverID INT PRIMARY KEY, Location VARCHAR(25), Status VARCHAR(7))")
+	cur.execute("CREATE TRIGGER ageCalc BEFORE INSERT ON Player FOR EACH ROW BEGIN SET NEW.age = (YEAR(CURDATE())-YEAR(NEW.DOB)); END;")
 
-	cur.execute("DROP TABLE IF EXISTS Player")
-	cur.execute("CREATE TABLE Player(gamerID VARCHAR(20) PRIMARY KEY, firstName VARCHAR(12), lastName VARCHAR(18), DOB DATE NOT NULL, age INT, gender VARCHAR(6))")
+	cur.execute("CREATE TRIGGER wLightAdd BEFORE INSERT ON equipedWeapons FOR EACH ROW BEGIN SET NEW.wLight = ((SELECT attack FROM PrimaryWeapons WHERE pName=NEW.pName)+(SELECT attack FROM SpecialWeapons WHERE sName=NEW.sName)+(SELECT attack FROM HeavyWeapons WHERE hName=NEW.hName)); END;")
 
+	cur.execute("CREATE TRIGGER aLightAdd BEFORE INSERT ON equipedArmor FOR EACH ROW BEGIN SET NEW.aLight = ((SELECT def FROM Helmet WHERE helmName=NEW.helmName AND class=NEW.class)+(SELECT def FROM Gauntlets WHERE gauntletName=NEW.gauntletName AND class=NEW.class)+(SELECT def FROM ChestArmor WHERE chestName=NEW.chestName AND class=NEW.class)+(SELECT def FROM LegArmor WHERE legName=NEW.legName AND class=NEW.class)); END;")
 
-	cur.execute("DROP TABLE IF EXISTS playerCharacter")
-	cur.execute("CREATE TABLE playerCharacter(race VARCHAR(6), gender VARCHAR(6), class VARCHAR(7), PRIMARY KEY (race, gender, class), light INT, strength INT, intelligence INT, discipline INT)")
+	cur.execute("CREATE TRIGGER lightCalc BEFORE INSERT ON pCharacter FOR EACH ROW BEGIN SET NEW.light = (((SELECT wLight FROM equipedWeapons WHERE gamerID=NEW.gamerID AND characterNum=NEW.characterNum)+(SELECT aLight FROM equipedArmor WHERE gamerID=NEW.gamerID AND characterNum=NEW.characterNum))/7); END;")
 
-	cur.execute("DROP TABLE IF EXISTS Vault")
-	cur.execute("CREATE TABLE Vault(storedArmor VARCHAR, storedWeapons VARCHAR, storedItems VARCHAR)")
-
-	cur.execute("DROP TABLE IF EXISTS LegArmor")
-	cur.execute("CREATE TABLE LegArmor(legName VARCHAR(20), class VARCHAR(7), PRIMARY KEY(legName, class), rarity VARCHAR(10), def INT, dis INT, inte INT, str INT)")
-
-	cur.execute("DROP TABLE IF EXISTS Gauntlets")
-	cur.execute("CREATE TABLE Gauntlets(gauntletName VARCHAR(20), class VARCHAR(7), PRIMARY KEY(gauntletName, class), rarity VARCHAR(10), def INT, dis INT, inte INT, str INT)")
-
-	cur.execute("DROP TABLE IF EXISTS ChestArmor")
-	cur.execute("CREATE TABLE ChestArmor(chestName  VARCHAR(20), class VARCHAR(7), PRIMARY KEY(chestName, class), rarity VARCHAR(10), def INT, dis INT, inte INT, str INT)")
-
-	cur.execute("DROP TABLE IF EXISTS Helmet")
-	cur.execute("CREATE TABLE Helmet(helmName VARCHAR(20), class VARCHAR(7), PRIMARY KEY(helmName, class), rarity VARCHAR(10), def INT, dis INT, inte INT, str INT)")
-
-	cur.execute("DROP TABLE IF EXISTS PrimaryWeapons")
-	cur.execute("CREATE TABLE PrimaryWeapons(pName VARCHAR(20) PRIMARY KEY, rarity VARCHAR(10), damageType VARCHAR(10), subtype VARCHAR(20), attack INT)")
-
-	cur.execute("DROP TABLE IF EXISTS SpecialWeapons")
-	cur.execute("CREATE TABLE SpecialWeapons(sName VARCHAR(20) PRIMARY KEY, rarity VARCHAR(10), damageType VARCHAR(10), subtype VARCHAR(20), attack INT)")
-
-	cur.execute("DROP TABLE IF EXISTS HeavyWeapons")
-	cur.execute("CREATE TABLE HeavyWeapons(hName VARCHAR(20) PRIMARY KEY, rarity VARCHAR(10), damageType VARCHAR(10), subtype VARCHAR(20), attack INT)")
-
-	cur.execute("DROP TABLE IF EXISTS Quests")
-	cur.execute("CREATE TABLE Quests(title VARCHAR(20) PRIMARY KEY, rewards VARCHAR, source VARCHAR)")
+	
